ef0b36a24052c10736ce332a949f20bcf3e4d4c1
Abiy Hussen Aman <AbiyHussen@gmail.com>
2022-12-14 05:51:21 -0800

    0 putchar is edited

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,32 +1,21 @@
 #include "main.h"
 /*
- * main - the first function called
- * 
- * Return: 0 always 
- *
- * */
+* main - the first function called
+*
+* Return: 0 always
+*/
 
 int main(void)
 {
-	_putchar(' ');
+_putchar(' ');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
 
-	_putchar('p');
-
-	_putchar('u');
-
-	_putchar('t');
-
-	_putchar('c');
-
-	_putchar('h');
-
-	_putchar('a');
-
-	_putchar('r');
-
-	_putchar('\n');
-
-	return (0);
-
-
+return (0);
 }
