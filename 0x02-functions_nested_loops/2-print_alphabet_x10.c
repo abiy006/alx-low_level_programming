cacf9a7f5462525a57df9b50a882cc8b58747418
Abiy Hussen Aman <AbiyHussen@gmail.com>
2022-12-14 10:37:10 -0800

    commit 1

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,10 +9,10 @@
 void print_alphabet_x10(void)
 {
 	char c;
-	int i=0;
-	while(i <= 0)
+	int i = 0;
+	while (i <= 0)
 	{
-		for(c = 'a'; c <= 'z'; c++)
+		for (c = 'a'; c <= 'z'; c++)
 		{
 			_putchar(c);
 		}
