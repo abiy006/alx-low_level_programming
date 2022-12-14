eb9f0be8d29e6ab25b5031713f2a89424d57a421
Abiy Hussen Aman <AbiyHussen@gmail.com>
2022-12-14 10:50:22 -0800

    commit 4

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,15 +1,16 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10 - print alphabet 10x 
- * Return void
+ * print_alphabet_x10 - print alphabet 10x
+ * Return: void
  *
- * */
+ */
 
 void print_alphabet_x10(void)
 {
 	char c;
 	int i = 0;
+
 	while (i <= 0)
 	{
 		for (c = 'a'; c <= 'z'; c++)
