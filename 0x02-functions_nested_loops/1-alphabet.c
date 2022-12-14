dba05d87174d430f91a964e3026cccdecbba7bb8
Abiy Hussen Aman <AbiyHussen@gmail.com>
2022-12-14 06:26:05 -0800

    first commit for project 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+* main - Entry point
+*
+* Description: 'print all the alphabets'
+*
+* Return: Always 0 (Success)
+*/
+
+void print_alphabet(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+	}
+	_putchar(\n);
+
+}
