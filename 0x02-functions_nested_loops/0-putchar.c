7187387cb3393ac93dac39e1f7f68d87f8b6c7e2
Abiy Hussen Aman <AbiyHussen@gmail.com>
2022-12-14 05:11:21 -0800

    seventh commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,16 @@
 #include "main.h"
-#include <unistd.h>
-/**                                                                                                                                                      
- *
- * _putchar - write the character c to stdout 
- * @c: the character to print
- *
- * Return: On success 
- */ 
-int _putchar(char c)
+int main(void)
 {
-	return (write(1, &c, 1));
+_putchar(' ');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+
+return (0);
+
 }
