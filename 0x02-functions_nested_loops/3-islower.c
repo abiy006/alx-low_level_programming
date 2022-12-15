f1e641aa5e2a8579200b7e57e8deae9a826620ae
Abiy Hussen Aman <AbiyHussen@gmail.com>
2022-12-15 04:37:07 -0800

    first commit on 3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *
+ * islower - check the character is lower or not
+ * Return: 1 if letter is lower else 0
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
