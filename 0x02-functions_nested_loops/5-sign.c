9c59622d0dbb850c1facc098ea7d6baf8dd07015
Abiy Hussen Aman <AbiyHussen@gmail.com>
2022-12-15 05:35:12 -0800

    first commit for task 5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_sign - checks the number sign and
+ *
+ * @n: is the character
+ *
+ * Return: 1 if n is grater than 0
+ * 0 if n is equal to 0
+ * -1 if n is less than 0
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		return (1);
+	}
+	else if (n == 0)
+	{
+		return (0);
+	}
+	else if (n < 0)
+	{
+		return (-1);
+	}
+}
