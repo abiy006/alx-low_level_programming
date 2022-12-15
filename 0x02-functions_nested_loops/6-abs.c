5e4b8b3e7e0c2df351708814e2087ea91e484afd
Abiy Hussen Aman <AbiyHussen@gmail.com>
2022-12-15 07:06:07 -0800

    fourth commit

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _abs - accept the value and return the absolute value
+ *
+ * @t: the character
+ *
+ * Return: absoulte value
+ */
+
+int _abs(int t)
+{
+	if (t >= 0)
+	{
+		return (t);
+	}
+	else
+	{
+		return (-t);
+	}
+}
+
