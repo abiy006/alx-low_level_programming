b1a72a771aab94c5e45f3957a296ac0a0f854b8e
Abiy Hussen Aman <AbiyHussen@gmail.com>
2022-12-17 04:30:31 -0800

    task 7 commit 1

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - staring
+ * Return: 0
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'z'; c >= 'a'; c--)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
