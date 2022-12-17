e46d1d2136f9694b81a8b07897a4b264fd77a816
Abiy Hussen Aman <AbiyHussen@gmail.com>
2022-12-17 04:47:21 -0800

    task 9 commit 1

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - starting
+ * Return: 0
+ */
+int main(void)
+{
+	int c;
+
+	for (c = 48; c < 58; c++)
+	{
+		putchar(c);
+		if (c != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+
+	putchar('\n');
+
+	return (0);
+	}
+}
