416fbd7829e469a4a009927bf505eb65eb10d1f9
Abiy Hussen Aman <AbiyHussen@gmail.com>
2022-12-22 06:14:06 -0800

    task 0 commit 3

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -11,11 +11,13 @@ char *_strcat(char *dest, char *src)
 	int i;
 	int j = 0;
 
-	while (dest[i++])
+	while (dest[j++])
 		j++;
 
-	for (i = 0; src[i]; i++)
-		dest[j++] = src[i];
+	for (i = 0; src[i] != '\0'; i++)
+		dest[j] = src[i];
+		j++;
 
+	dest[j] = '\0';
 	return (dest);
 }
