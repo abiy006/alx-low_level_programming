18787ebdc1700cdad77ebb78acdee0e36b8cd910
Abiy Hussen Aman <AbiyHussen@gmail.com>
2022-12-22 05:25:44 -0800

    my first commit on project 0x06

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * strcat - starting ...
+ * @src: source string 
+ * @dest: destination string ...
+ * Return: pointer to @dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i, j = 0;
+
+	while (dest[i++])
+		j++;
+
+	for (i = 0; src[i]; i++)
+		dest[j++] = src[i];
+
+	return (dest);
+}
---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This is project 0x06 Readme file
