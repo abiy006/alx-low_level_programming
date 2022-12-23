966c24cc734dec437563682b2eea6fac2b26e624
Abiy Hussen Aman <AbiyHussen@gmail.com>
2022-12-23 05:36:39 -0800

    task 10 commit 2

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
-
+/**
+ * main - starting
+ *
+ * Return: 0
+ */
 int main(void)
 
 {
@@ -7,10 +11,12 @@ int main(void)
 	int a[5];
 	int *p;
 
-	a[2] = 1024;
+
+	a[2] = 1024
 	p = &n;
-	/*
- * write your line of code here...
+
+/*
+ * Write your line of code here...
  * Remember:
  * - you are not allowed to use a
  * - you are not allowed to modify p
@@ -18,7 +24,7 @@ int main(void)
  * - you are not allowed to code anything else than this line of code
  */
 *(p + 5) = 98;
-	/* ...so that this prints 98\n */
-	printf("a[2] = %d\n", a[2]);
-	return (0);
+/* ...so that this prints 98\n */
+printf("a[2] = %d\n", a[2]);
+return (0);
 }
