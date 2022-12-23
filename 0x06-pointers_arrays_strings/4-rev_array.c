8131a8d3af6afb0220b6c6fa40c46836f9e36422
Abiy Hussen Aman <AbiyHussen@gmail.com>
2022-12-23 00:48:00 -0800

    task 4 commit 1

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * reverse_array - starting ...
+ * @a: array
+ * @n: integer
+ * Return: nothing
+ */
+
+void reverse_array(int *a, int n)
+{
+	int i, j;
+
+	for (i = n - 1; i > n / 2; i--)
+	{
+		j = a[n - 1 - i];
+		a[n - 1 - i] = a[i];
+		a[i] = j;
+}
