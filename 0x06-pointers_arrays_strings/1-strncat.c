fdcf5ef46b8c31ee8ff6d18da9e15aafde56fa28
Abiy Hussen Aman <AbiyHussen@gmail.com>
2022-12-22 23:59:43 -0800

    task 1 commit 1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strncat - starting ...
+ * @src: source string
+ * @dest: destination string ...
+ * @n: int
+ * Return: string
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0;
+	int j = 0;
+
+	while (dest[i++])
+		j++;
+
+	for (i = 0; src[i] && i < n; i++)
+		dest[j++] = src[i];
+
+	return (dest);
+}
