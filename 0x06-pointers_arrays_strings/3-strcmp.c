53b43144df9f0cf6875c4a71541accd771bbc7e1
Abiy Hussen Aman <AbiyHussen@gmail.com>
2022-12-23 00:23:31 -0800

    task 3 commit 2

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -18,12 +18,12 @@ int _strcmp(char *s1, char *s2)
 			break;
 		else if (s1[i] == '\0')
 		{
-			j == s2[i];
+			j = s2[i];
 			break;
 		}
 		else if (s2[i] == '\0')
 		{
-			j == s1[i];
+			j = s1[i];
 			break;
 		}
 		else if (s1[i] != s2[i])
