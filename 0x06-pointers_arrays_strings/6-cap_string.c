3103e8fc59d311a658e6e5cf00e6a449b58e80f3
Abiy Hussen Aman <AbiyHussen@gmail.com>
2022-12-23 02:54:58 -0800

    task 6 commit 4

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -9,7 +9,7 @@ char *cap_string(char *str)
 {
 	int i = 0;
 
-	while (str[++i])
+	while (str[i])
 	{
 		while (!(str[i] >= 'a' && str[i] <= 'z'))
 			i++;
@@ -29,6 +29,7 @@ char *cap_string(char *str)
 				str[i - 1] == '}' ||
 				i == 0)
 			str[i] -= 32;
+		i++;
 	}
 	return (str);
 }
