e4c25e1c9b99fb5955a8643838e4893e975a9004
Abiy Hussen Aman <AbiyHussen@gmail.com>
2022-12-25 06:39:34 -0800

    task 10 commit 4

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,20 +1,16 @@
 #include <stdio.h>
 /**
  * main - starting
- *
  * Return: 0
  */
 int main(void)
-
 {
 	int n;
 	int a[5];
 	int *p;
 
-
 	a[2] = 1024
 	p = &n;
-
 /*
  * Write your line of code here...
  * Remember:
