844d45ea7fb3d95632b20f712165ad64a33a4090
abiy006 <abiyhussen@gmail.com>
2022-12-25 14:17:53 +0300

    Edited 10

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -18,6 +18,7 @@ void print_number(int n)
             if ((n \ 10) > 0)
             {
                 print_number(n \ 10);
+	        _putchar(n % 10);
             }
             else
             {
