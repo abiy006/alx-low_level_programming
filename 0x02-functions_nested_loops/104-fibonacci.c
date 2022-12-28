2e9832efedf28f1d5bd6fcac02c716dc941ffce6
Abiy Hussen Aman <AbiyHussen@gmail.com>
2022-12-28 03:43:14 -0800

    edited 43

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -5,23 +5,23 @@
  */
 int main(void)
 {
-unsigned long int i = 1;
-unsigned long int j = 2;
+uint64_t i = 1;
+uint64_t j = 2;
 int f = 0;
-unsigned long int h, k1, k2;
+uint64_t h, k1, k2;
 
 	while (f < 98)
 	{
 		if (f == 0)
 		{
-			printf("%ld", i);
+			printf("%lu", i);
 			printf(", ");
 		}
 		else if (f == 1)
 		{
 			k1 = i;
 			k2 = j;
-			printf("%ld", j);
+			printf("%lu", j);
 			printf(", ");
 		}
 		else
@@ -29,7 +29,7 @@ unsigned long int h, k1, k2;
 			h = k1 + k2;
 			k1 = k2;
 			k2 = h;
-			printf("%ld", h);
+			printf("%lu", h);
 
 			if (f != 97)
 			{
