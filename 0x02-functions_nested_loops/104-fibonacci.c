993fceab4851a11957e5a9cad8a39b0cde4b9af6
Abiy Hussen Aman <AbiyHussen@gmail.com>
2022-12-28 04:19:41 -0800

    edited 46

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -8,10 +8,17 @@ int main(void)
 long double i = 1;
 long double j = 2;
 int f = 0;
-long double h, k1, k2;
 
 	while (f < 98)
 	{
+		long double c1, c2, h;
+
+		if ((f != 1) || (f != 2) || (f != 3))
+		{
+		c1 = c2;
+		c2 = h;
+		}
+
 		if (f == 0)
 		{
 			printf("%.0Lf", i);
@@ -19,16 +26,14 @@ long double h, k1, k2;
 		}
 		else if (f == 1)
 		{
-			k1 = i;
-			k2 = j;
+			c1 = i;
+			c2 = j;
 			printf("%.0Lf", j);
 			printf(", ");
 		}
 		else
 		{
-			h = k1 + k2;
-			k1 = k2;
-			k2 = h;
+			h = c1 + c2;
 			printf("%.0Lf", h);
 
 			if (f != 97)
