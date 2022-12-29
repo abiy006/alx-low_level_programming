a2476c3227f9c9597eee3ad68804407ae2297bff
Abiy Hussen Aman <AbiyHussen@gmail.com>
2022-12-29 02:59:05 -0800

    Project 3 task 1 commit 1

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * main - starting ...
+ * Return: 0
+ */
+int main(void)
+{
+	int i = 0;
+
+	postitive_or_negative(i);
+	return (0);
+}
---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,6 @@
+#include "main.h"
+/**
+ * main - starting ...
+ * Return: 0
+ */
+
