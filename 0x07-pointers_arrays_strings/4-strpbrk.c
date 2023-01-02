a6142c0680d0847d8a43b33727a31c65bc79837b
Abiy Hussen Aman <AbiyHussen@gmail.com>
2023-01-02 04:51:02 -0800

    Project 7 - Task 4 - commit 1

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strpbrk - searches a string
+ * @s: string ...
+ * @accept: string ...
+ * Return:  a pointer or null...
+ */
+
+char _strpbrk(char *s, char *accept)
+{
+unsigned int i = 0;
+
+while (*s)
+{
+
+	for (i = 0; accept[i]; i++)
+	{
+		if (accept[i] == *s)
+			return (s);
+	}
+s++;
+}
+return ('\0');
+}
