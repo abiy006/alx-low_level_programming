d44d487fd99dd54193b9518e377d11a38c091ed8
Abiy Hussen Aman <AbiyHussen@gmail.com>
2023-01-04 03:15:50 -0800

    Project 8 - Task 5 - commit 02

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,17 +1,30 @@
 #include "abiy.h"
 
 /**
- * _sqrt_recursion - return the square of a number
- * @n: interger ...
- * Return: integer ...
- **/
+ * _myloop - return x the power of y
+ * @x: value to raise
+ * @y: power of
+ * Return: result of the power
+ */
 
+int _myloop(int x, int y)
+{
+	if (x * x == y)
+		return (x);
+	else if (x * x > y)
+		return (-1);
+	else
+		return (x * _myloop(x + 1, y));
+}
+
+/**
+ * _sqrt_recursion - return integer
+ * @n: integer
+ * Return: result of the power
+ */
 int _sqrt_recursion(int n)
 {
-	if (n < 0)
-		return (-1);
-	else if (n == 0)
-		return (1);
-	else
-		return (n * _sqrt_recursion(n - 1));
+	int i = 1;
+
+	return (_myloop(i, n));
 }
