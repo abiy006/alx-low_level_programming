cd9e7b56ac3f6d78799aa341406d04500604ac0c
Abiy Hussen Aman <AbiyHussen@gmail.com>
2023-01-04 05:49:21 -0800

    Project 8 - Task 100 - commit 1

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,16 @@
+#include "abiy.h"
+
+/**
+ * is_palindrome -return is string is a palindrome
+ * @s: string ...
+ * Return: integer ...
+ **/
+
+int is_palindrome(char *s)
+{
+	int i = 0;
+
+	if (*s == i)
+		return (0);
+	return (1);
+}
