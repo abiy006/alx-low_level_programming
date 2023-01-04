b43d185ab42278787ebfd99565cf40faa451f82c
Abiy Hussen Aman <AbiyHussen@gmail.com>
2023-01-04 03:32:36 -0800

    Project 8 - Task 6 - commit 01

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,30 @@
+#include "abiy.h"
+
+/**
+ * _myloop - return x the power of y
+ * @x: value to raise
+ * @y: power of
+ * Return: result of the prime
+ */
+
+int _myloop(int x, int y)
+{
+	if (y == 1)
+		return (1);
+	else if (x % y == 0 && y > 0)
+		return (0);
+	else
+		return (_myloop(x, y - 1));
+}
+
+/**
+ * is_prime_number - return integer
+ * @n: integer
+ * Return: result of the prime
+ */
+int is_prime_number(int n)
+{
+	if (n < 2)
+		return (1);
+	return (_myloop(n, n - 1));
+}
