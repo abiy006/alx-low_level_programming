f1fd7b63fee886c4648c5ce82c3c787d97df0cb4
Abiy Hussen Aman <AbiyHussen@gmail.com>
2023-01-03 23:41:19 -0800

    Project 8 - Task 1 - commit 1

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,13 @@
+#include "abiy.h"
+
+/**
+ * _print_rev_recursion - prints a string in reverse
+ * @s: string ...
+ **/
+
+void _print_rev_recursion(char *s)
+{
+	if (*s)
+		_print_rev_recursion(s++);
+		_putchar(*s);
+}
