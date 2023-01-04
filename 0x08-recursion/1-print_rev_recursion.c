f4796d09bab995dca88733163536ff9f581d5dde
Abiy Hussen Aman <AbiyHussen@gmail.com>
2023-01-04 00:02:31 -0800

    Project 8 - Task 1 - commit 7

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -7,13 +7,12 @@
 
 void _print_rev_recursion(char *s)
 {
-	if (*s)
+	int i = 0;
+
+	while (*s)
 	{
 		_print_rev_recursion(++s);
 		_putchar(*s);
-		if (*s == '\0')
-		{
-			_putchar('\n');
-		}
 	}
+	_putchar('\n');
 }
