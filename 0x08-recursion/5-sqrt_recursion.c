67114c94046131578f7fa38339e2bf3fc7e03228
Abiy Hussen Aman <AbiyHussen@gmail.com>
2023-01-04 00:59:21 -0800

    Project 8 - Task 5 - commit 1

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,17 @@
+#include "abiy.h"
+
+/**
+ * _sqrt_recursion - return the square of a number
+ * @n: interger ...
+ * Return: integer ...
+ **/
+
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	else if (n == 0)
+		return (1);
+	else
+		return (n * _pow_recursion(n, n - 1));
+}
