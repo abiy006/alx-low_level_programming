d9622e45724f3e79f34bafa28d3c85f26c3d166e
Abiy <abiyhussen@gmail.com>
2023-01-10 15:30:01 +0300

    Project 0x0B-malloc_free - commit 1

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,26 @@
+#include "abiy.h"
+
+/**
+ * create_array - creates an array of chars, and initializes it with a specific char.
+ * @size: size of the array
+ * @c: the initialized charactor 
+ *
+ * Return: NULL if size is 0
+ * otherwise Returns a pointer to the array, or NULL if it fails
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *array;
+
+	if (!size)
+		return (NULL);
+
+	array = (char *) malloc(sizeof(char) * size);
+
+	if (array)
+	{
+		while (size > 0)
+			array[--size] = c;
+	}
+	return (array);
+}
