426b9ccbbb2c297b6f065222139175c965f8e148
Abiy <abiyhussen@gmail.com>
2023-01-11 16:28:19 +0300

    Project A - Task 2 - Commit 1

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include "abiy.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main -  print name
+ *@argi: int.
+ *@argj: char.
+ * Return: Always 0.
+ */
+int main(int argi, char *argj[])
+{
+	int a;
+
+	for (a = 0 ; a < argi ; a++)
+	{
+		printf("%s\n", argj[i]);
+	}
+	return (0);
+}
