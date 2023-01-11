7b1f4e02dc5433673861699e4086c55679b3c226
Abiy <abiyhussen@gmail.com>
2023-01-11 15:46:23 +0300

    Project A - Task 1 - Commit 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,25 @@
+#include "abiy.h"
+
+/**
+ * main - Prints the number of arguments passed into main
+ * @arg1: Number of command line arguments
+ * @arg2: Array name
+ * Return: 0
+ */
+
+int main(int argc1, char *argv2[])
+{
+	int a;
+
+	if (arg1 == 1)
+		printf("%d\n", arg1 - 1);
+	else
+	{
+		for (a = 0; *arg2; a++, arg2++)
+			;
+
+		printf("%d\n", a - 1);
+	}
+
+	return (0);
+}
