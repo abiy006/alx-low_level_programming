e36c17b86b1dacccb21ebaf0dff287fcb2ecafbf
Abiy <abiyhussen@gmail.com>
2023-01-12 19:23:39 +0300

    Project C - Task 0 - Commit 9

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,18 +1,20 @@
 #include "abiy.h"
-#include <stdio.h>
-#include <stdlib.h>
+
 /**
- * malloc_checked - creates an array of integers.
- * @b: input.
- * Return: Output.
+ * malloc_checked - allocates memory.
+ * @b: the size to allocate.
+ *
+ * Return: pointer to allocated memory or 98 if failed.
  */
+
 void *malloc_checked(unsigned int b)
 {
-	void *g = malloc(b);
+	char *ptr;
 
-	if (g == NULL)
-	{
+	ptr = malloc(b);
+	if (ptr == NULL)
 		exit(98);
-	}
-	return (g);
+
+	return (ptr);
+
 }
