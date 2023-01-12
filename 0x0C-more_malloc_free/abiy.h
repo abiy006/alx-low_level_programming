3d440af1473227ff03d6aff630247ded75be6b58
Abiy <abiyhussen@gmail.com>
2023-01-12 18:39:19 +0300

    abiy.h file is edited

--- 0x0C-more_malloc_free/abiy.h
+++ 0x0C-more_malloc_free/abiy.h
@@ -1,9 +1,6 @@
 #ifndef ABIY_H
 #define ABIY_H
 
-#include <stdio.h>
-#include <stdlib.h>
-
 int _putchar(char c);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
