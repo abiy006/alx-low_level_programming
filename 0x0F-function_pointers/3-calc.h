935b7b7fd545304ab19aa30ffb9603fc88f4313a
Abiy <abiyhussen@gmail.com>
2023-01-19 16:17:20 +0300

    task 3 files are modified

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,13 +1,5 @@
-#include "3-calc.h"
-#include <stdio.h>
-#include <stdlib.h>
-
-int (*get_op_func(char *s))(int, int);
-int op_add(int a, int b);
-int op_sub(int a, int b);
-int op_mul(int a, int b);
-int op_div(int a, int b);
-int op_mod(int a, int b);
+#ifndef CALC_H
+#define CALC_H
 
 /**
  * struct op - Struct op
@@ -20,5 +12,11 @@ typedef struct op
 	char *op;
 	int (*f)(int a, int b);
 } op_t;
+
+int op_add(int a, int b);
+int op_sub(int a, int b);
+int op_mul(int a, int b);
+int op_div(int a, int b);
+int op_mod(int a, int b);
 
 #endif
